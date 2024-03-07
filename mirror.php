f1dbd3429fa375c2394f45201aeebc1b1e4d193c
Jordi Boggiano <j.boggiano@seld.be>
2024-03-07 11:59:33 +0100

    Update to php 8.1+, update deps, add types to everything and add phpstan config

--- mirror.php
+++ mirror.php
@@ -2,6 +2,8 @@
 <?php
 
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\HttpClient\Exception\TransportException;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Lock\LockFactory;
@@ -29,19 +31,24 @@ set_error_handler(function ($errno, $errstr, $errfile, $errline) {
 });
 
 class Mirror {
-    private $target;
-    private $context;
-    private $userAgent;
-    private $verbose;
-    private $statsdSocket;
-    private $client;
-    private $url;
-    private $apiUrl;
-    private $hostname;
-    private $gzipOnly;
-    private $syncRootOnV2;
-    private $downloaded = 0;
+    private string $target;
+    private string $userAgent;
+    private bool $verbose;
+    /**
+     * @var open-resource|null
+     */
+    private mixed $statsdSocket = null;
+    private HttpClientInterface $client;
+    private string $url;
+    private string $apiUrl;
+    private string $hostname;
+    private bool $gzipOnly;
+    private bool $syncRootOnV2;
+    private int $downloaded = 0;
 
+    /**
+     * @param array<mixed> $config
+     */
     public function __construct(array $config)
     {
         $this->target = $config['target_dir'];
@@ -62,7 +69,7 @@ class Mirror {
         }
     }
 
-    public function syncRootOnV2()
+    public function syncRootOnV2(): void
     {
         if (!$this->syncRootOnV2) {
             return;
@@ -81,8 +88,8 @@ class Mirror {
             return;
         }
 
-        $gzipped = gzencode($rootData, 8);
-        $this->write('/packages.json', $rootData, $gzipped, strtotime($rootResp->getHeaders()['last-modified'][0]));
+        $gzipped = $this->gzencode($rootData, 8);
+        $this->write('/packages.json', $rootData, $gzipped, $this->strtotime($rootResp->getHeaders()['last-modified'][0]));
         $this->output('X');
 
         $this->statsdIncrement('mirror.sync_root');
@@ -100,7 +107,7 @@ class Mirror {
         throw new \Exception('Failed to fetch timestamp from API, got invalid response '.$resp->getStatusCode().': '.$resp->getContent());
     }
 
-    public function syncV2()
+    public function syncV2(): bool
     {
         $this->initClient();
 
@@ -108,7 +115,7 @@ class Mirror {
         $this->downloaded = 0;
 
         if (!file_exists($this->getTimestampStorePath())) {
-            if (!is_writable(getcwd())) {
+            if (!is_writable((string) getcwd())) {
                 throw new \UnexpectedValueException('Cannot save last timestamp to last_metadata_timestamp in '.getcwd().'. Make sure the directory is writable.');
             }
 
@@ -116,7 +123,10 @@ class Mirror {
         } elseif (!is_writable($this->getTimestampStorePath())) {
             throw new \UnexpectedValueException('Cannot save last timestamp to last_metadata_timestamp in '.getcwd().'. Make sure the file is writable.');
         }
-        $lastTime = trim(file_get_contents($this->getTimestampStorePath()));
+        $lastTime = trim((string) file_get_contents($this->getTimestampStorePath()));
+        if ($lastTime === '') {
+            throw new \RuntimeException('Failed loading the last timestamp from '.$this->getTimestampStorePath());
+        }
 
         $changesResp = $this->client->request('GET', $this->apiUrl.'/metadata/changes.json?since='.$lastTime, ['headers' => ['Host' => parse_url($this->apiUrl, PHP_URL_HOST)]]);
         if ($changesResp->getHeaders()['content-encoding'][0] !== 'gzip') {
@@ -140,7 +150,7 @@ class Mirror {
                 // package here can be foo/bar or foo/bar~dev, not strictly a package name
                 $pkg = $action['package'];
                 $provPathV2 = '/p2/'.$pkg.'.json';
-                $headers = file_exists($this->target.$provPathV2.'.gz') ? ['If-Modified-Since' => gmdate('D, d M Y H:i:s T', filemtime($this->target.$provPathV2.'.gz'))] : [];
+                $headers = file_exists($this->target.$provPathV2.'.gz') ? ['If-Modified-Since' => gmdate('D, d M Y H:i:s T', (int) filemtime($this->target.$provPathV2.'.gz'))] : [];
                 $userData = ['path' => $provPathV2, 'minimumFilemtime' => $action['time'], 'retries' => 0];
                 $requests[] = ['GET', $this->url.$provPathV2, ['user_data' => $userData, 'headers' => $headers]];
             } elseif ($action['type'] === 'delete') {
@@ -160,7 +170,7 @@ class Mirror {
         return true;
     }
 
-    public function resync(int $timestamp)
+    public function resync(int $timestamp): bool
     {
         $this->output('Resync requested'.PHP_EOL);
 
@@ -176,9 +186,13 @@ class Mirror {
             $names = array_flip($list['packageNames']);
 
             foreach ($finder as $vendorDir) {
-                foreach (glob(((string) $vendorDir).'/*.json.gz') as $file) {
+                $globMatches = glob(((string) $vendorDir).'/*.json.gz');
+                if (!is_array($globMatches)) {
+                    throw new \RuntimeException('Failed globbing '.$vendorDir.'/*.json.gz');
+                }
+                foreach ($globMatches as $file) {
                     if (!preg_match('{/([^/]+/[^/]+?)(~dev)?\.json.gz$}', strtr($file, '\\', '/'), $match)) {
-                        throw new \LogicException('Could not match package name from '.$path);
+                        throw new \LogicException('Could not match package name from '.$file);
                     }
 
                     if (!isset($names[$match[1]])) {
@@ -192,7 +206,7 @@ class Mirror {
 
                     // check for corrupted file if it is older than the bugfix date, and remove if they are corrupted
                     if (filemtime((string) $file) < 1606297354) {
-                        $contents = json_decode(gzdecode(file_get_contents((string) $file)), true);
+                        $contents = $this->decodeFile((string) $file);
                         if (!isset($contents['packages'][$match[1]])) {
                             unlink((string) $file);
                             // also remove the version without .gz suffix if it exists
@@ -238,7 +252,7 @@ class Mirror {
         return true;
     }
 
-    private function getTimestampStorePath()
+    private function getTimestampStorePath(): string
     {
         $timestampStore = __DIR__.'/last_metadata_timestamp';
 
@@ -251,21 +265,24 @@ class Mirror {
         return $timestampStore;
     }
 
-    private function writeLastTimestamp(int $timestamp)
+    private function writeLastTimestamp(int $timestamp): void
     {
         if (false === file_put_contents($this->getTimestampStorePath(), $timestamp)) {
             throw new \UnexpectedValueException('Could not save last timestamp to last_metadata_timestamp in '.getcwd().'. Make sure the file/directory is writable');
         }
     }
 
-    private function downloadV2Files(array $requests)
+    /**
+     * @param array<array{string, string, array<mixed>}> $requests
+     */
+    private function downloadV2Files(array $requests): bool
     {
         $hasRetries = false;
 
         $responseNeedsRetry = function ($response, array $userData) use (&$hasRetries, &$requests): bool {
             $is404 = $response->getStatusCode() === 404;
             if (!$is404) {
-                $mtime = strtotime($response->getHeaders(false)['last-modified'][0]);
+                $mtime = $this->strtotime($response->getHeaders(false)['last-modified'][0]);
             }
 
             // got an outdated file, possibly fetched from a mirror which was not yet up to date, so retry after 2sec
@@ -281,7 +298,7 @@ class Mirror {
                 $this->output('R');
                 $this->statsdIncrement('mirror.retry_provider_v2');
                 $userData['retries']++;
-                $headers = file_exists($this->target.$userData['path'].'.gz') ? ['If-Modified-Since' => gmdate('D, d M Y H:i:s T', filemtime($this->target.$userData['path'].'.gz'))] : [];
+                $headers = file_exists($this->target.$userData['path'].'.gz') ? ['If-Modified-Since' => gmdate('D, d M Y H:i:s T', (int) filemtime($this->target.$userData['path'].'.gz'))] : [];
                 $requests[] = ['GET', $this->url.$userData['path'], ['user_data' => $userData, 'headers' => $headers]];
 
                 return true;
@@ -299,7 +316,7 @@ class Mirror {
             $this->output('E');
             $this->statsdIncrement('mirror.retry_provider_v2_error');
             $userData['retries']++;
-            $headers = file_exists($this->target.$userData['path'].'.gz') ? ['If-Modified-Since' => gmdate('D, d M Y H:i:s T', filemtime($this->target.$userData['path'].'.gz'))] : [];
+            $headers = file_exists($this->target.$userData['path'].'.gz') ? ['If-Modified-Since' => gmdate('D, d M Y H:i:s T', (int) filemtime($this->target.$userData['path'].'.gz'))] : [];
             array_unshift($requests, ['GET', $this->url.$userData['path'], ['user_data' => $userData, 'headers' => $headers]]);
 
             return true;
@@ -384,8 +401,8 @@ class Mirror {
                             continue;
                         }
 
-                        $mtime = strtotime($response->getHeaders()['last-modified'][0]);
-                        $gzipped = gzencode($metadata, 7);
+                        $mtime = $this->strtotime($response->getHeaders()['last-modified'][0]);
+                        $gzipped = $this->gzencode($metadata, 7);
                         $this->write($userData['path'], $metadata, $gzipped, $mtime);
                         $this->output('M');
                         $this->statsdIncrement('mirror.sync_provider_v2');
@@ -411,7 +428,7 @@ class Mirror {
         return true;
     }
 
-    public function sync()
+    public function sync(): bool
     {
         $this->initClient();
 
@@ -468,7 +485,7 @@ class Mirror {
                 $requests[] = ['GET', $this->url.$provPath, ['user_data' => $userData]];
             }
 
-            $listingsToWrite['/'.$listing] = [$listingData, strtotime($listingResp->getHeaders()['last-modified'][0])];
+            $listingsToWrite['/'.$listing] = [$listingData, $this->strtotime($listingResp->getHeaders()['last-modified'][0])];
         }
 
         while ($requests) {
@@ -502,8 +519,8 @@ class Mirror {
                             throw new \Exception('Invalid hash received for file '.$userData[0]);
                         }
 
-                        $mtime = strtotime($response->getHeaders()['last-modified'][0]);
-                        $gzipped = gzencode($providerData, 7);
+                        $mtime = $this->strtotime($response->getHeaders()['last-modified'][0]);
+                        $gzipped = $this->gzencode($providerData, 7);
                         $this->write($userData[0], $providerData, $gzipped, $mtime);
                         $this->write($userData[1], $providerData, $gzipped, $mtime);
                         $this->output('P');
@@ -522,14 +539,14 @@ class Mirror {
         }
 
         foreach ($listingsToWrite as $listing => $listingData) {
-            $gzipped = gzencode($listingData[0], 8);
+            $gzipped = $this->gzencode($listingData[0], 8);
             $this->write($listing, $listingData[0], $gzipped, $listingData[1]);
             $this->output('L');
             $this->statsdIncrement('mirror.sync_listing');
         }
 
-        $gzipped = gzencode($rootData, 8);
-        $this->write('/packages.json', $rootData, $gzipped, strtotime($rootResp->getHeaders()['last-modified'][0]));
+        $gzipped = $this->gzencode($rootData, 8);
+        $this->write('/packages.json', $rootData, $gzipped, $this->strtotime($rootResp->getHeaders()['last-modified'][0]));
         $this->output('X');
         $this->statsdIncrement('mirror.sync_root');
 
@@ -539,7 +556,7 @@ class Mirror {
         return true;
     }
 
-    public function gc()
+    public function gc(): void
     {
         // GC is only for v1 metadata, so abort if v1 is not enabled
         if ($this->syncRootOnV2) {
@@ -553,13 +570,13 @@ class Mirror {
         if (!file_exists($rootFile)) {
             return;
         }
-        $rootJson = json_decode(gzdecode(file_get_contents($rootFile)), true);
+        $rootJson = $this->decodeFile($rootFile);
 
         foreach ($rootJson['provider-includes'] as $listing => $opts) {
             $listing = str_replace('%hash%', $opts['sha256'], $listing).'.gz';
             $safeFiles['/'.$listing] = true;
 
-            $listingJson = json_decode(gzdecode(file_get_contents($this->target.'/'.$listing)), true);
+            $listingJson = $this->decodeFile($this->target.'/'.$listing);
             foreach ($listingJson['providers'] as $pkg => $opts) {
                 $provPath = '/p/'.$pkg.'$'.$opts['sha256'].'.json.gz';
                 $safeFiles[$provPath] = true;
@@ -569,7 +586,10 @@ class Mirror {
         $this->cleanOldFiles($safeFiles);
     }
 
-    private function cleanOldFiles(array $safeFiles)
+    /**
+     * @param array<string, true> $safeFiles
+     */
+    private function cleanOldFiles(array $safeFiles): void
     {
         $finder = Finder::create()->directories()->ignoreVCS(true)->in($this->target.'/p');
         foreach ($finder as $vendorDir) {
@@ -579,7 +599,7 @@ class Mirror {
                 ->in((string) $vendorDir);
 
             foreach ($vendorFiles as $file) {
-                $key = strtr(str_replace($this->target, '', $file), '\\', '/');
+                $key = strtr(str_replace($this->target, '', (string) $file), '\\', '/');
                 if (!isset($safeFiles[$key])) {
                     unlink((string) $file);
                     // also remove the version without .gz suffix if it exists
@@ -593,7 +613,7 @@ class Mirror {
         // clean up old provider listings
         $finder = Finder::create()->depth(0)->files()->name('provider-*.json.gz')->ignoreVCS(true)->in($this->target.'/p')->date('until 10minutes ago');
         foreach ($finder as $provider) {
-            $key = strtr(str_replace($this->target, '', $provider), '\\', '/');
+            $key = strtr(str_replace($this->target, '', (string) $provider), '\\', '/');
             if (!isset($safeFiles[$key])) {
                 unlink((string) $provider);
                 // also remove the version without .gz suffix if it exists
@@ -604,21 +624,28 @@ class Mirror {
         }
     }
 
-    private function output($str)
+    private function output(string $str): void
     {
         if ($this->verbose) {
             echo $str;
         }
     }
 
-    private function getHash($file)
+    private function getHash(string $file): string|null
     {
         if (file_exists($this->target.$file)) {
-            return hash_file('sha256', $this->target.$file);
+            $hash = hash_file('sha256', $this->target.$file);
+            if (false === $hash) {
+                throw new \RuntimeException('Failed hashing file '.$this->target.$file);
+            }
+
+            return $hash;
         }
+
+        return null;
     }
 
-    private function download($file)
+    private function download(string $file): ResponseInterface
     {
         $this->downloaded++;
 
@@ -641,7 +668,7 @@ class Mirror {
         return $resp;
     }
 
-    private function write($file, $content, $gzipped, $mtime)
+    private function write(string $file, string $content, string $gzipped, int $mtime): void
     {
         $path = $this->target.$file;
 
@@ -659,7 +686,7 @@ class Mirror {
         rename($path.'.gz.tmp', $path.'.gz');
     }
 
-    private function delete($packageName)
+    private function delete(string $packageName): void
     {
         $this->output('D');
         $files = [
@@ -675,7 +702,7 @@ class Mirror {
         }
     }
 
-    public function statsdIncrement($metric)
+    public function statsdIncrement(string $metric): void
     {
         static $loggedStatsdError = null;
 
@@ -692,7 +719,7 @@ class Mirror {
         }
     }
 
-    private function statsdConnect(string $ip, int $port)
+    private function statsdConnect(string $ip, int $port): void
     {
         try {
             $socket = fsockopen('udp://' . $ip, $port, $errno, $errstr, 1);
@@ -716,9 +743,9 @@ class Mirror {
         $this->statsdSocket = $socket;
     }
 
-    private function initClient()
+    private function initClient(): void
     {
-        if ($this->client) {
+        if (isset($this->client)) {
             return;
         }
 
@@ -731,6 +758,40 @@ class Mirror {
             'max_duration' => 120,
             'http_version' => '2.0',
         ]);
+    }
+
+    private function decodeFile(string $path): mixed
+    {
+        $contents = file_get_contents($path);
+        if (false === $contents) {
+            throw new \RuntimeException('Failed reading '.$path);
+        }
+        $contents = gzdecode($contents);
+        if (false === $contents) {
+            throw new \RuntimeException('Failed gzdecoding '.$path);
+        }
+
+        return json_decode($contents, true);
+    }
+
+    private function gzencode(string $data, int $level): string
+    {
+        $gzipped = gzencode($data, $level);
+        if (false === $gzipped) {
+            throw new \RuntimeException('Failed gzencoding '.$data);
+        }
+
+        return $gzipped;
+    }
+
+    private function strtotime(string $str): int
+    {
+        $timestamp = strtotime($str);
+        if (false === $timestamp) {
+            throw new \RuntimeException('Failed parsing time '.$str);
+        }
+
+        return $timestamp;
     }
 }
 
@@ -808,7 +869,9 @@ try {
     }
 } catch (\Throwable $e) {
     // sleep so supervisor assumes a correct start and we avoid restarting too quickly, then rethrow
-    $mirror->statsdIncrement('mirror.hard_failure');
+    if (isset($mirror)) {
+        $mirror->statsdIncrement('mirror.hard_failure');
+    }
     sleep(3);
     echo 'Mirror '.($isV2 ? 'v2' : '').' job failed at '.date('Y-m-d H:i:s').PHP_EOL;
     echo '['.get_class($e).'] '.$e->getMessage().PHP_EOL;
